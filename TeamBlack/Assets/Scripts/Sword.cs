47e7b28dd897b9867bf9d6f1e96a9e97b43ccd23
Ralleg314 <ralleg314@gmail.com>
2016-03-16 11:04:48 +0100

    Unitats d'atac i classes d'unitat

---
+++ TeamBlack/Assets/Scripts/Sword.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sword : AttackUnits
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        mana = 10;
+        //Dir = inDir;
+        HP = 200.0f;
+        Att = 10.0f;
+        Def = 5.0f;
+        Speed = 0.5f;
+        AttSp = 1.0f;
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+        if (Attacking != null)
+        {
+            countdown -= Time.deltaTime;
+            if (Attacking.getHP() > 0 && countdown <= 0.0f)
+            {
+                getRigidBody().velocity = Vector2.zero;
+                Attacking.getAttacked(Att);
+                countdown = AttSp;
+            }
+        }
+        else
+        {
+            //Move
+        }
+    }
+}
