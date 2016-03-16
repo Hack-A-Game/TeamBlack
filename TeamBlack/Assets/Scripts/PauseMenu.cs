d1e94a422b6138d3439d16670f9764c7988ceaa7
laizpablo <laizpablo@hotmail.com>
2016-03-16 13:12:04 +0100

    commit pause menu

--- TeamBlack/Assets/Scripts/PauseMenu.cs
+++ TeamBlack/Assets/Scripts/PauseMenu.cs
@@ -6,6 +6,7 @@ public class PauseMenu : MonoBehaviour
 
     public void onClick()
     {
-        //Controller.Controller.desactiveGamePaused();
+        Controller.controller.desactiveGamePaused();
+        Debug.Log("holaa");
     }
 }
