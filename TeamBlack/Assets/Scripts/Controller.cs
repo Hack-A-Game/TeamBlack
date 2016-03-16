efe5d38378dfe96bfa7e4deaa728ea7185896296
laizpablo <laizpablo@hotmail.com>
2016-03-16 12:47:50 +0100

    controller

--- TeamBlack/Assets/Scripts/Controller.cs
+++ TeamBlack/Assets/Scripts/Controller.cs
@@ -6,16 +6,20 @@ using UnityEngine.SceneManagement;
 public class Controller : MonoBehaviour {
     private Player _player1;
     private Player _player2;
-    private bool _startplayer1Attack;
     private Map _mapa;
     private bool _gamePause;
-    enum TurnPlayer1 { Attack, Defense };
-    enum TurnPlayer2 { Attack, Defense };
+    public enum TurnPlayer1 { Attack, Defense };
+    public enum TurnPlayer2 { Attack, Defense };
     enum MenuScreen { Menu, Game, Options};
     private TurnPlayer1 turnPlayer1;
     private TurnPlayer2 turnPlayer2;
     private float countdown = 30.0f;
     private bool endTime;
+
+    public UnityEngine.UI.Button myButtonStart;
+    public UnityEngine.UI.Button myButtonPlayer1;
+    public UnityEngine.UI.Button myButtonPlayer2;
+    public UnityEngine.UI.Button myButtonRandom;
 
     // Use this for initialization
     void Start () {
@@ -23,19 +27,26 @@ public class Controller : MonoBehaviour {
         _player2 = new Player();
         _gamePause = false;
         endTime = false;
+        myButtonStart.enabled = true;
+        myButtonPlayer1.enabled = false;
+        myButtonPlayer2.enabled = false;
+        myButtonRandom.enabled = false;
+        myButtonPlayer1.gameObject.SetActive(false);
+        myButtonPlayer2.gameObject.SetActive(false);
+        myButtonRandom.gameObject.SetActive(false);
+        DontDestroyOnLoad(transform.gameObject);
 
-	}
+}
 
-    void Update()
+void Update()
     {
         countdown -= Time.deltaTime;
         if (countdown <= 0.0f)
             endTime = true;
-        Debug.Log(countdown);
                 
     }
 
-    public void defineTurn(){
+    public void defineTurn(TurnPlayer1 p1, TurnPlayer2 p2){
         turnPlayer1 = TurnPlayer1.Defense;
         turnPlayer2 = TurnPlayer2.Attack;
     }
@@ -53,7 +64,7 @@ public class Controller : MonoBehaviour {
         }
     }
 
-    public Player getTurnAttack(){
+    public Player getPlayerAttack(){
         if (turnPlayer1 == TurnPlayer1.Attack)
         {
             return _player1;
@@ -64,7 +75,7 @@ public class Controller : MonoBehaviour {
         }
     }
 
-    public Player getTurnDefense()
+    public Player getPlayerDefense()
     {
         if (turnPlayer1 == TurnPlayer1.Defense)
         {
@@ -80,6 +91,49 @@ public class Controller : MonoBehaviour {
     {
         SceneManager.LoadScene(nameScene);
     }
+
+    public void startGame()
+    {
+        myButtonStart.enabled = false;
+        myButtonPlayer1.gameObject.SetActive(true);
+        myButtonPlayer2.gameObject.SetActive(true);
+        myButtonRandom.gameObject.SetActive(true);
+
+        myButtonStart.gameObject.SetActive(false);
+        myButtonPlayer1.enabled = true;
+        myButtonPlayer2.enabled = true;
+        myButtonRandom.enabled = true;
+        Debug.Log("StartGame");
+    }
+
+    public void playerStarts(string choice)
+    {
+        Debug.Log(choice);
+        if (choice.Equals("p1"))
+        {
+            defineTurn(TurnPlayer1.Defense, TurnPlayer2.Attack);
+        }
+        else if (choice.Equals("p2"))
+        {
+            defineTurn(TurnPlayer1.Attack, TurnPlayer2.Defense);
+        }
+        else if (choice.Equals("p3"))
+        {
+            defineTurn(TurnPlayer1.Defense, TurnPlayer2.Attack);
+        }
+        loadScene("GamePlay");
+    }
+
+    public void activeGamePaused()
+    {
+        _gamePause = !_gamePause;
+    }
+
+    public void desactiveGamePaused()
+    {
+        _gamePause = !_gamePause;
+    }
+
 }
 
 
