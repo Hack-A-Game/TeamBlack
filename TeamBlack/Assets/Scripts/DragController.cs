0c0af2afc8e3878aff980e9ed0452db2b1a51776
gpascualg <gpascualg93@gmail.com>
2016-03-16 10:19:40 +0100

    WIP Draggable

---
+++ TeamBlack/Assets/Scripts/DragController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragController : MonoBehaviour
+{
+    private GameObject _hitGameobject;
+
+	// Use this for initialization
+	void Start()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+	    if (Input.GetMouseButtonDown(0))
+        {
+            Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition))
+        }
+	}
+}
---
+++ TeamBlack/Assets/Scripts/Draggable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Draggable : MonoBehaviour
+{
+    bool _isDragging = false;
+
+	// Use this for initialization
+	void Start()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        
+	}
+}
