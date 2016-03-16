5629683542c6f54f7d213397f3518c4bfccb172d
Ralleg314 <ralleg314@gmail.com>
2016-03-16 18:45:50 +0100

    Flag runner

---
+++ TeamBlack/Assets/Scripts/Units/FlagRunner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NewBehaviourScript : Unit {
+
+	// Use this for initialization
+	public override void Start () {
+        base.Start();
+
+        mana = 10;
+        //Dir = inDir;
+        HP = 15.0f;
+        Att = 20.0f;
+        MAXHP = HP;
+        Def = 3.0f;
+        Speed = 120f;
+        AttSp = 0.05f;
+        encounterList = new List<Unit>();
+        range = 1.5f;
+        canCapture = true;
+    }
+	
+}
