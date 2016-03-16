4105d720420290659f5fc69dc725b752e4c5cd68
laizpablo <laizpablo@hotmail.com>
2016-03-16 08:37:31 +0100

    controller

--- TeamBlack/Assets/Scripts/Controller.cs
+++ TeamBlack/Assets/Scripts/Controller.cs
@@ -2,11 +2,35 @@
 using System.Collections;
 
 public class Controller : MonoBehaviour {
+    private Player _player1;
+    private Player _player2;
+    private bool _player1Start;
+    //private static Mapa mapa;
 
 	// Use this for initialization
 	void Start () {
-	
+        _player1 = new Player();
+        _player2 = new Player();
+        randomStartPlayer();
 	}
 	
+    public void randomStartPlayer(){
+        Debug.Log(Random.Range(1,10));
+        if (Random.Range(1, 10)%2==0) {
+            _player1Start = true;
+        }
+        else {
+            _player1Start = false;
+        }
+
+    }
+    public void startPlayer1(){
+        _player1Start = true;
+    }
+
+    public void startPlayer2(){
+        _player1Start = false;
+    }
+    
 
 }
