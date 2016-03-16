58a5409061a670a3adeec46dcb6b65c5e8df2966
Pau Ortega <pauortega993@gmail.com>
2016-03-16 18:59:51 +0100

    BAndera y el espia, el espia esta vacio

---
+++ TeamBlack/Assets/Scripts/Bandera.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bandera : MonoBehaviour {
+    public float HP;
+    private Player player;
+    // Use this for initialization
+    void Start () {
+        HP = 100.0f;
+        player = Controller.controller.getPlayerAttack();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (HP <= 0)
+        {
+            player.points += 5;
+        }
+	
+	}
+    public void getAttacked(float damage)
+    {
+
+        HP -= damage;
+
+    }
+
+    public double getHP()
+    {
+        return HP;
+    }
+}
