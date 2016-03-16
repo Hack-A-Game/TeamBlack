330a63a3597cb9b271aa427642c844dd771494d4
Pau Ortega <pauortega993@gmail.com>
2016-03-16 11:07:52 +0100

    Monster classes

---
+++ TeamBlack/Assets/Scripts/Golem.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Golem : Unit {
+
+	// Use this for initialization
+	void Start () {
+        HP = 250.0f;
+        Att = 25.0f;
+        Def = 10.0f;
+        AttSp = 2.0f;
+        mana = 5;
+
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+
+
+        if (Attacking != null)
+        {
+            countdown -= Time.deltaTime;
+            if (Attacking.getHP > 0 && countdown <= 0.0f)
+            {
+                Attacking.getAttacked(Att);
+                countdown = AttSp;
+            }
+
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "AttackUnit")
+        {
+            Attacking = coll.gameObject.GetComponent<Unit>();
+        }
+
+
+    }
+}
