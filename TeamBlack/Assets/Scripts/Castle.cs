d095d93fd896f53c2c5bbcb513bda1c8680b39d2
lazaro92 <lazaroalbert92@gmail.com>
2016-03-16 10:33:08 +0100

    Clase palyer con mana y limites de unidades y clase castillo

---
+++ TeamBlack/Assets/Scripts/Castle.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Castle : MonoBehaviour
+{
+
+    private Rigidbody2D rb2d;
+    private BoxCollider2D bc2d;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        bc2d = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Detect the colision between the Castle and Monster
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Monster"))
+        {
+            Debug.Log("> Colision Monster - Castle");
+        }
+    }
+}
